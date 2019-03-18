16a51dcfe4b4361e2123dd3e4f4c8ea601ee9e5c
JosephRArias <31783008+JosephRArias@users.noreply.github.com>
2019-03-18 11:07:00 -0300

    Add files via upload

---
+++ Dummy/Dummy/StringCalc/StringCalc.Tests/StringCalcTest.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCalc.Tests
+{
+	[TestFixture]
+    public class StringCalcTest
+    {
+		[Test]
+		public void EmptyStringEqualsZero()
+		{
+			//arrange
+			var calculator = new StringCalc();
+			//act
+			int res = calculator.Add("");
+			//assert
+			Assert.AreEqual(0, res);
+		}
+		[Test]
+		public void SingleStringEqualsNumber()
+		{
+			//arrange
+			var calculator = new StringCalc();
+			//act
+			int res = calculator.Add("2");
+			//assert
+			Assert.AreEqual(2, res);
+		}
+		[Test]
+		public void NumberPlusNumberEqualsAnotherNumber()
+		{
+			//arrange
+			var calculator = new StringCalc();
+			//act
+			calculator.Parse("3,2");
+			int res = calculator.Add("3,2");
+			//assert
+			Assert.AreEqual(5, res);
+		}
+    }
+}
---
+++ Dummy/Dummy/StringCalc/StringCalc/StringCalc.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCalc
+{
+	public class StringCalc
+	{
+		int num = 0;
+		int result = 0;
+		public int Add(string v)
+		{
+			string[] numbers = v.Split(',');
+			if (v.Length == 0)
+			{
+				num = 0;
+			}
+			else
+			{
+				for(int i = 0; i < numbers.Length; i++)
+				{
+					num += int.Parse(numbers[i]);
+				}
+			}
+			return num;
+		}
+
+		public void Parse(string v)
+		{
+			bool parsable = int.TryParse(v, out result);
+			if (parsable)
+			{
+				Add(v);
+			}
+		}
+	}
+}
